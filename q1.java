381b4c40cc422adaab2d95173353160316b2dc91
ash23996 <60687760+ash23996@users.noreply.github.com>
2020-02-18 16:30:05 +0530

    Delete q1.java

--- q1.java
+++
@@ -1,8 +0,0 @@
-public class q1 {
-    public static void main(String args[]) {
-        String str="Ashita Kumar";
-        System.out.println( str.replace( 'h','k' ) );
-        System.out.println( str.replaceFirst("sh", "hs") );
-        System.out.println( str.replaceAll("Ku", "at") );
-    }
-}
